24ec809a2785be1d6fd4f03411e468c14d0d5bad
rraajjeevv <renascence_being@yahoo.com>
2017-06-06 12:48:54 +0100

    Add files via upload

---
+++ 20170606_createtables_dummymvc.sql
@@ -0,0 +1,67 @@
+﻿use `dummymvc`;
+
+/*
+DROP TABLE IF EXISTS `dummymvc`.`aspnetuserroles`;
+DROP TABLE IF EXISTS `dummymvc`.`aspnetuserlogins`;
+DROP TABLE IF EXISTS `dummymvc`.`aspnetuserclaims`;
+DROP TABLE IF EXISTS `dummymvc`.`aspnetusers`;
+DROP TABLE IF EXISTS `dummymvc`.`aspnetroles`;
+*/
+
+
+CREATE TABLE  `aspnetroles` (
+  `Id` varchar(128) NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB ;
+
+
+CREATE TABLE  `aspnetusers` (
+  `Id` varchar(128) NOT NULL,
+  `Email` varchar(256) DEFAULT NULL,
+  `EmailConfirmed` tinyint(1) NOT NULL,
+  `PasswordHash` longtext,
+  `SecurityStamp` longtext,
+  `PhoneNumber` longtext,
+  `PhoneNumberConfirmed` tinyint(1) NOT NULL,
+  `TwoFactorEnabled` tinyint(1) NOT NULL,
+  `LockoutEndDateUtc` datetime DEFAULT NULL,
+  `LockoutEnabled` tinyint(1) NOT NULL,
+  `AccessFailedCount` int(11) NOT NULL,
+  `UserName` varchar(256) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB ;
+
+
+CREATE TABLE  `aspnetuserclaims` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` varchar(128) NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Id` (`Id`),
+  KEY `UserId` (`UserId`),
+  CONSTRAINT `FK_aspnetuserclaims_1` FOREIGN KEY (`UserId`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB ;
+
+
+CREATE TABLE  `aspnetuserlogins` (
+  `LoginProvider` varchar(128) NOT NULL,
+  `ProviderKey` varchar(128) NOT NULL,
+  `UserId` varchar(128) NOT NULL,
+  PRIMARY KEY (`LoginProvider`,`ProviderKey`,`UserId`),
+  KEY `ApplicationUser_Logins` (`UserId`),
+  CONSTRAINT `FK_aspnetuserlogins_1` FOREIGN KEY (`UserId`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB ;
+
+
+
+
+CREATE TABLE  `aspnetuserroles` (
+  `UserId` varchar(128) NOT NULL,
+  `RoleId` varchar(128) NOT NULL,
+  PRIMARY KEY (`UserId`,`RoleId`),
+  KEY `IdentityRole_Users` (`RoleId`),
+  CONSTRAINT `FK_aspnetuserroles_1` FOREIGN KEY (`UserId`) REFERENCES `aspnetusers` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `FK_aspnetuserroles_2` FOREIGN KEY (`RoleId`) REFERENCES `aspnetroles` (`Id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB ;
